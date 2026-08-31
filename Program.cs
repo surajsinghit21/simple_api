c1be163ad8f3ea4dcda6752f1d223d34f986ea87
surajsinghit21 <surajsinghit21@gmail.com>
2026-08-31 21:04:18 +0530

    test commit 1

--- Program.cs
+++ Program.cs
@@ -16,7 +16,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
